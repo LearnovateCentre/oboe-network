ed880decf8df98798170e310cedb0f5682a0bd42
Pablo.Alvarez <pablo.alvarez@learnovatecentre.org>
2023-08-17 13:40:03 +0200

    Setting CORS whitelists just in case

--- server/index.ts
+++ server/index.ts
@@ -15,12 +15,28 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config(); //
 const app = express();
+var allowedOrigins = ['http://localhost:3000',
+                      'https://localhost:3001',
+                      'https://oboe-network1.service.local:3001',
+                      "https://oboe-network-1.kmk10bfmaomnk.eu-west-1.cs.amazonlightsail.com"];
 
 // middlewares
 app.use(morgan("dev"));
 app.use(express.json({ limit: "30mb"})); // for parsing application/json
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // handling errors
 
